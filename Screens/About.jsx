474f0d1495c1d3302b92b5f878137154315c8b51
Chanteng <yrnchanteng@gmail.com>
2022-11-23 04:18:35 -0800

    otp added

--- Screens/About.jsx
+++ Screens/About.jsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import OTP from '../components/OTP'
 
 const About = () => {
   return (
     <View>
-      <Text>About</Text>
+   <OTP />
     </View>
   )
 }
